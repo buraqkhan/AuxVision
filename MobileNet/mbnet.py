2e7b1037575df93a60bf9df6ef9d28418b247d24
Ali Asghar <aliasghar1070@gmail.com>
2021-12-13 12:41:06 +0500

    Updated MobileNet

--- MobileNet/mbnet.py
+++ MobileNet/mbnet.py
@@ -1,39 +1,130 @@
 import cv2
-
-thres = 0.45
-
-cap = cv2.VideoCapture(0)
-#cap.set(3,1280)
-#cap.set(4,720)
-#cap.set(10,70)
+import time
+import queue
+import numpy as np
+from threading import Thread
+from queue import Queue
 
 classNames= []
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
     classNames=[line.rstrip() for line in f]
-
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'frozen_inference_graph.pb'
+weightsPath = 'frozen_inference_graph.pb'   
 
-net = cv2.dnn_DetectionModel(weightsPath,configPath)
-net.setInputSize(320,320)
-net.setInputScale(1.0/ 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
 
-while True:
-    success,img = cap.read()
-    classIds, confs, bbox = net.detect(img,confThreshold=thres)
-    print(classIds,bbox)
+def InputFramesThread(inputQueue,outputQueue,model,thres):
+    model[0] = cv2.dnn_DetectionModel(weightsPath,configPath)
+    model[0].setInputSize(320,320)
+    model[0].setInputScale(1.0/ 127.5)
+    model[0].setInputMean((127.5, 127.5, 127.5))
+    model[0].setInputSwapRB(True)
+    while True:
+        img = inputQueue.get()
+        model[1], model[2], model[3] = model[0].detect(img,confThreshold=thres)
+        outputQueue.put(img)
 
-    if len(classIds) != 0:
-        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            print(box[0], box[1])
-            #cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-            #cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+def getObjects(coordinatesList):
+    objectsDetected = dict()
+    # print(coordList[-5:])
+    # RecentCoordinates = coordList[-20:]
+    for coordinate in coordinatesList:
+        # print(coordinate)
+        if len(coordinate) != 0:
+            if coordinate[4] not in objectsDetected:
+                objectsDetected[coordinate[4]] = 1
+            else:
+                objectsDetected[coordinate[4]] += 1
+        
+    # objectsDetected = dict(sorted(objectsDetected.items(), key=lambda item: item[1]))
+    mostFrequentObjects = []
 
-    cv2.imshow('Output',img)
-    cv2.waitKey(1)
+    for key,value in objectsDetected.items():
+        # print(key,value)
+        if value > 15:
+            mostFrequentObjects.append(key)
+
+    return mostFrequentObjects
+
+def getObjectsCoordinates(coordinatesList):
+    recentCoordinates = []
+    objectCoordinates = []
+    mostFrequentObjects = getObjects(coordinatesList)
+    for coordinate in coordinatesList:
+        # print(coordinate)
+        if len(coordinate) != 0:
+            if coordinate[4] in mostFrequentObjects:
+                recentCoordinates.append(coordinate)
+    for object in mostFrequentObjects:
+        for coordinate in reversed(recentCoordinates):
+            if coordinate[4] == object:
+                objectCoordinates.append(coordinate)
+                break
+    coordinatesList.clear()
+    # print(mostFrequentObjects)
+    # print(objectCoordinates)
+
+    return objectCoordinates
+
+
+def getCoordinates():
+    thres = 0.6
+    cap = cv2.VideoCapture(0)  
+    #cap.set(3,1280)
+    #cap.set(4,720)
+    #cap.set(10,70) 
+    inputQueue = Queue(2)
+    outputQueue = Queue()
+    coordinates = []
+    objects = []
+    net = None
+    classIds = None
+    confs = None
+    bbox = None
+    model = []
+    model.append(net)
+    model.append(classIds)
+    model.append(confs)
+    model.append(bbox)
+    inputThread = Thread(target=InputFramesThread, args= (inputQueue,outputQueue,model,thres))
+    inputThread.daemon = True
+    inputThread.start()
+    while True:
+        startTime = time.time()
+        success,img = cap.read()
+        inputQueue.put(img)
+        
+        if outputQueue.empty():
+            pass
+        else:
+            # print(type(model[0]))
+            # print(type(model[1]))
+            # print(type(model[2]))
+            # print(type(model[3]))
+            tempImg = outputQueue.get()
+            if len(model[1]) != 0:
+                # print("Num of BBox : ", len(model[3]))
+                for classId, confidence,box in zip(model[1].flatten(),model[2].flatten(),model[3]):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    coordinates.append(tuple((box[0],box[1],box[2],box[3],classNames[classId-1])))
+                    if (len(coordinates) % 60 == 0):
+                        objects = getObjectsCoordinates(coordinates)
+                        # print("60 coordinates found!")
+                        print(objects)
+                        # print("All objects printed")
+
+                    #cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    #cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            endTime = time.time()
+            totalTime = endTime - startTime
+            fps = 1 / totalTime
+            # print("FPS: ", fps)
+            cv2.putText(img, f'FPS: {int(fps)}', (20,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            cv2.imshow('Output',img)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    break
+
+coordinate = getCoordinates()
