70c225962af7a17ed48fa6d361dfb461db2c9d23
buraqk <buraqkhan18@gmail.com>
2021-12-17 10:33:42 +0500

    updated integration.py

--- integration.py
+++ integration.py
@@ -23,14 +23,28 @@ Use openmp for threading? Update: Openmp doesn't exist in python
 #     distance = 420 + (1.3 * average) + (0.00168 * average**2)
 #     return (name, distance)  
 
+# def getAvgDistance(x1, y1, x2, y2, name):
+    # print(x1, x2, y1, y2)
+    # bounding_box = depth_video.disparity[x1:x2, y1:y2]
+    # mean = np.mean(bounding_box)
+    # std_dev = np.std(bounding_box)
+    # print(std_dev)
+    # dist_mean = abs(bounding_box - mean)
+    # max_dev = 1
+    # no_outliers = dist_mean < max_dev * (std_dev/2)
+    # bounding_box = bounding_box[no_outliers]
+    # average = np.mean(bounding_box)
+    # mid_x = (x1 + x2)/2
+    # mid_y = (y1 + y2)/2
+    # average = depth_video.disparity[int(mid_x), int(mid_y)]
+    # print(average)
+    # distance = 420 + (1.3 * average) + (0.00168 * average**2)
+    # return (name, distance)
+
 def getAvgDistance(x1, y1, x2, y2, name):
-    bounding_box = depth_video.disparity[x1:x2, y1:y2]
-    mean = np.mean(bounding_box)
-    std_dev = np.std(bounding_box)
-    dist_mean = abs(bounding_box - mean)
-    max_dev = 1
-    no_outliers = dist_mean < max_dev * std_dev
-    bounding_box = bounding_box[no_outliers]
+    mid_x = int((x1 + x2)/2)
+    mid_y = int((y1 + y2)/2)
+    bounding_box = depth_video.disparity[mid_x-50:mid_x+50, mid_y-50:mid_y+50]
     average = np.mean(bounding_box)
     distance = 420 + (1.3 * average) + (0.00168 * average**2)
     return (name, distance)
