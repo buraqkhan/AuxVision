161e96d923b3fab83ba4ca39fbbcce1fa375b788
buraqk <buraqkhan18@gmail.com>
2022-05-06 21:10:03 +0500

    updated equation

--- integration.py
+++ integration.py
@@ -19,24 +19,6 @@ import cv2
 #     average = np.median(bounding_box)
 #     distance = 420 + (1.3 * average) + (0.00168 * average**2)
 #     return (name, distance)  
-
-# def getAvgDistance(x1, y1, x2, y2, name):
-    # print(x1, x2, y1, y2)
-    # bounding_box = depth_video.disparity[x1:x2, y1:y2]
-    # mean = np.mean(bounding_box)
-    # std_dev = np.std(bounding_box)
-    # print(std_dev)
-    # dist_mean = abs(bounding_box - mean)
-    # max_dev = 1
-    # no_outliers = dist_mean < max_dev * (std_dev/2)
-    # bounding_box = bounding_box[no_outliers]
-    # average = np.mean(bounding_box)
-    # mid_x = (x1 + x2)/2
-    # mid_y = (y1 + y2)/2
-    # average = depth_video.disparity[int(mid_x), int(mid_y)]
-    # print(average)
-    # distance = 420 + (1.3 * average) + (0.00168 * average**2)
-    # return (name, distance)
 ''' =============================================================================   '''
  
 def getAvgDistance(x1, y1, x2, y2, name):
@@ -44,7 +26,7 @@ def getAvgDistance(x1, y1, x2, y2, name):
     mid_y = int((y1 + y2)/2)
     bounding_box = depth_video.disparity[mid_x-50:mid_x+50, mid_y-50:mid_y+50]
     average = np.mean(bounding_box)
-    distance = 420 + (1.3 * average) + (0.00168 * average**2)
+    distance = 691 + (-1.59 * average) + (0.00116 * average**2)
     return (name, distance)
 
 def getBoundingBoxes(cap_right):
