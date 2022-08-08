18f3192b1e369de37c6e3c2419393517903bccdf
buraqk <buraqkhan18@gmail.com>
2022-08-08 17:38:55 +0500

    testing new stereo code

--- stereo/test_depth_video.py
+++ stereo/test_depth_video.py
@@ -89,6 +89,46 @@ def stereo_depth_map(rectified_pair):
         quit();
     # return disparity_color
 
+def depth_map(imgL, imgR):
+    """ Depth map calculation. Works with SGBM and WLS. Need rectified images, returns depth map ( left to right disparity ) """
+    # SGBM Parameters -----------------
+    window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+
+    left_matcher = cv2.StereoSGBM_create(
+        minDisparity=-1,
+        numDisparities=5*16,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+        blockSize=window_size,
+        P1=8 * 3 * window_size,
+        # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+        P2=32 * 3 * window_size,
+        disp12MaxDiff=12,
+        uniquenessRatio=10,
+        speckleWindowSize=50,
+        speckleRange=32,
+        preFilterCap=63,
+        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+    )
+    right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
+    # FILTER Parameters
+    lmbda = 80000
+    sigma = 1.3
+    visual_multiplier = 6
+
+    wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+    wls_filter.setLambda(lmbda)
+
+    wls_filter.setSigmaColor(sigma)
+    displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
+    dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
+    displ = np.int16(displ)
+    dispr = np.int16(dispr)
+    filteredImg = wls_filter.filter(displ, imgL, None, dispr)  # important to put "imgL" here!!!
+
+    filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
+    filteredImg = np.uint8(filteredImg)
+
+    return filteredImg
+
 def load_map_settings( fName ):
     global SWS, PFS, PFC, MDS, NOD, TTH, UR, SR, SPWS, loading_settings
     print('Loading parameters from file...')
@@ -137,6 +177,11 @@ while(cap_right.isOpened() and cap_left.isOpened()):
     imgL_new = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
     imgR_new = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
     
-    rectified_pair = (imgL_new, imgR_new)
-    stereo_depth_map(rectified_pair)
+    #rectified_pair = (imgL_new, imgR_new)
+    #stereo_depth_map(rectified_pair)
+    img_new = depth_map(imgL, imgR)
+    cv2.imshow("Image", img_new)
+    key = cv2.waitKey(1) & 0xFF   
+    if key == ord("q"):
+    	quit();
         #print(disparity[0][0])
